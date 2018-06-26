3e4db69a836b3153539b9708be83d01c54fb7080
Lindsey Booth <lindseyboth@yahoo.com>
2018-06-26 14:10:38 -0500

    Updates

---
+++ excercise.py
@@ -0,0 +1,7 @@
+name = input('What is your name?')
+sentence = "Hello %s! Your name has %s letters in it! Awesome!"%(name, len(name))
+print(sentence.upper())
+
+subject = input("What is your facovorite subject in school?")
+sentence2 = "%s's favorite subject in school is %s."%(name, subject)
+print(sentence2)
