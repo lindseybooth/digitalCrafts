e43788afa23b0b3f1d48cd9103810d6ba71e7eff
Lindsey Booth <lindseyboth@yahoo.com>
2018-06-28 11:53:17 -0500

    List Excercises HW

---
+++ listExcercises.py
@@ -0,0 +1,73 @@
+# #1
+# myNum = [1, 224, 4234, 2, 4, 6, -3453, 2, 43, 5, -23, 347, 5, -17]
+# print(sum(myNum))
+
+# #2
+# print(max(myNum))
+
+# #3
+# print(min(myNum))
+
+# #4
+# for number in myNum:
+#     if number % 2 == 0:
+#         print(number)
+
+# #5
+# for number in myNum:
+#     if number >= 0:
+#         print(number)
+
+# #6
+# newNum = [n for n in myNum if n > 0]
+# print(newNum)
+
+# #7
+
+# newNum1 = []
+# for i in myNum:
+#     newNum1.append(i*3)
+# print(newNum1)
+
+#8
+# list1 = [2, 4, 5]
+# list2 = [2, 3, 6]
+
+# list2index = 0
+
+# for list1index in list1:
+#     print(list1index * list2[list2index])
+#     list2index += 1
+
+#9
+matrix1 = [1, 3, 2, 4]
+matrix2 = [5, 2, 1, 0]
+
+matrixIndex = 0
+
+for matrix1Index in matrix1:
+  print(matrix1Index + matrix2[matrixIndex])
+  matrixIndex +=1
+
+#10
+matrix1 = [1, 3, 2, 4]
+matrix2 = [5, 2, 1, 0]
+
+matrixIndex = 0
+
+for matrix1Index in matrix1:
+  print(matrix1Index + matrix2[matrixIndex])
+  matrixIndex +=1
+
+#11
+# myList = [1, 4, 6, 4, 345, 78, 345, 1, 2, 7, 2, 1]
+# newList =[]
+
+# for number in myList:
+#   if number not in newList:
+#     newList.append(number)
+# print(newList)
+
+
+
+
