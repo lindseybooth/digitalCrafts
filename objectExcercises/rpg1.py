66c3f04821a031e9fde60356bab3a26b416f5f36
Lindsey Booth <lindseyboth@yahoo.com>
2018-07-02 13:57:47 -0500

    RPG 1 Step 1

---
+++ objectExcercises/rpg1.py
@@ -0,0 +1,11 @@
+class Goblin:
+    
+    def __init__ (self, health, power):
+        self.health = 6
+        self.power = 2
+
+class Hero:
+    
+    def def __init__ (self, health, power):
+        self.health = 10
+        self.power = 5
